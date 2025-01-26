e84ce8fd5f4f4cd9891385ce0124e758d11b99bc
saravananatgithub <38750533+saravananatgithub@users.noreply.github.com>
2025-01-26 12:20:58 +0100

    Add files via upload

---
+++ C1.java
@@ -0,0 +1,13 @@
+public class C1 {
+
+	public static void main(String[] args) {
+		//sop
+		System.out.println(" Inside C1");
+		int age = 45;
+		String name = "Saravanan";
+		String address = "Scwhalbach";
+		System.out.println("age is:" + age);
+		System.out.println("name is:" + name);
+	}
+
+}
